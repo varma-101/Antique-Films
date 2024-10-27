2ae59bb07fd7757788cd78c954ba82831feba2e2
SAGI VENKATA NAGA GOPAL VARMA <varmasagi101@gmail.com>
2024-10-27 13:33:17 +0530

    Image

--- frontend/src/home/Hero.jsx
+++ frontend/src/home/Hero.jsx
@@ -6,13 +6,16 @@ const Hero = () => {
   return (
     <Carousel>
       <Carousel.Item>
-        <img src="images/newborn.png" className="d-block w-100" alt="First slide" />
+        <img src="images/image1.png" className="d-block w-100" alt="First slide" />
       </Carousel.Item>
       <Carousel.Item>
-        <img src="images/newborn.png" className="d-block w-100" alt="Second slide" />
+        <img src="images/image2.jpg" className="d-block w-100" alt="Second slide" />
       </Carousel.Item>
       <Carousel.Item>
-        <img src="images/newborn.png" className="d-block w-100" alt="Third slide" />
+        <img src="images/image3.png" className="d-block w-100" alt="Third slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="images/image4.jpg" className="d-block w-100" alt="Third slide" />
       </Carousel.Item>
     </Carousel>
   );
