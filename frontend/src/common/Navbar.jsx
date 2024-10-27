f59a4f07226c625620b4108b895a8821fa8d0ffb
SAGI VENKATA NAGA GOPAL VARMA <varmasagi101@gmail.com>
2024-10-27 13:23:24 +0530

    Navbar Adjustments

--- frontend/src/common/Navbar.jsx
+++ frontend/src/common/Navbar.jsx
@@ -3,20 +3,22 @@ import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
-    <div className='nav-component'>
+    <div className="nav-component">
       <div className="navbar">
-        <div className='logo'>
-          <p>ANTIQUE FILMS</p>
+        <div className="logo">
+          <img src="images/logo.png" alt="" />
         </div>
-        <div className='nav'>
+        <div className="nav">
           <ul>
             <li>Home</li>
             <li>About</li>
             <li>Occasions</li>
             <li>Testimonials</li>
             <li>Gallery</li>
-            <li><button>Let's Talk</button></li>
           </ul>
+        </div>
+        <div className="nav-button">
+          <button>Let's Talk</button>
         </div>
       </div>
     </div>
