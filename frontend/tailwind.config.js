54340313324af3a37dfd3222535c8a9d059d3e82
SAGI VENKATA NAGA GOPAL VARMA <varmasagi101@gmail.com>
2024-10-29 01:27:04 +0530

    Added tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // This includes all JavaScript and TypeScript files in src and its subdirectories
+    './public/index.html', // If you use Tailwind CSS classes directly in your HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
