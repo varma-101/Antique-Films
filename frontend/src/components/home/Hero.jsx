f3874a81ea7c8ac32796c075483e7a9776f4acc3
SAGI VENKATA NAGA GOPAL VARMA <varmasagi101@gmail.com>
2025-01-31 09:00:32 +0530

    responsive 1.0

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -5,10 +5,9 @@ function Hero() {
         <div className="hero">
             <img src="images/image.png" alt="" height={850} width={800} className="cam"/>
             <div className="hero-right">
-                <h1 className="hero-head">“Capture Timeless Moments with <br/>Our Photography Services.”</h1>
-                <p className="hero-content">Welcome to Antique Films, where we specialize in capturing elegant and professional photography that immortalizes your precious moments.Our team of skilled photographers uses high-quality equipment and creative vision to deliver stunning images that exceed expectations.  <br /><br />
-                  <button className="chat-btn"> <span style={{fontSize:"1.5rem",paddingTop:"1500px",fontWeight:"bolder"}}><i class="fa-brands fa-whatsapp"></i></span> Chat with us</button>
-                </p>
+                <h1 className="hero-head">“Capture Timeless Moments with <br/>Our Photography Services.”</h1> <br />
+                <p className="hero-content">Welcome to Antique Films, where we specialize in capturing elegant and professional photography that immortalizes your precious moments.Our team of skilled photographers uses high-quality equipment and creative vision to deliver stunning images that exceed expectations.</p>
+                <button className="chat-btn"> <span style={{fontSize:"1.5rem",paddingTop:"1500px",fontWeight:"bolder"}}><i class="fa-brands fa-whatsapp"></i></span> <span className="chat">Chat with us</span></button>
             </div>
         </div>
       </>
